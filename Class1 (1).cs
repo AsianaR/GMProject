2a9bb803b8ee23bc15a60e8822a057c43cad4f13
AsianaR <46110409+AsianaR@users.noreply.github.com>
2020-02-05 14:13:50 +0200

    Add files via upload

---
+++ Class1 (1).cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            var queue = new Queue<long>();
+
+     
+            queue.Enqueue(380991234567);//3
+            queue.Enqueue(380919485718);//2
+            queue.Enqueue(380445105858);//1
+            
+            Console.WriteLine(queue.Count);
+            var item1 = queue.Dequeue();
+            Console.WriteLine(item1);//1
+
+            queue.Enqueue(72410242185);//new 3
+
+            var item4 = queue.Peek();
+            Console.WriteLine(item4);
+           
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program (1).cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Queue
+{
+    
+    public class Queue<T>
+    {       
+        private List<T> resitems = new List<T>();
+
+        public int Count => resitems.Count;
+
+        public void Enqueue(T item)
+        {
+            resitems.Add(item);
+        }
+
+        public T Dequeue()
+        {
+            var item = GetItem();
+            resitems.Remove(item);
+            return item;
+        }
+
+        public T Peek()
+        {
+            var item = GetItem();
+            return item;
+        }
+
+        private T GetItem()
+        {
+            var item = resitems.LastOrDefault();
+            return item;
+        }
+    }
+}
